70b07dbfc3e8b96a535fea03cdbd718954f7c9c9
Greg Trevellick <gtrevellick@googlemail.com>
2019-06-02 17:44:39 +0100

    .

--- vsixArtefacts/VsmpPublish.cmd
+++ vsixArtefacts/VsmpPublish.cmd
@@ -1,9 +1,13 @@
-# https://docs.microsoft.com/en-us/visualstudio/extensibility/walkthrough-publishing-a-visual-studio-extension-via-command-line?view=vs-2017
+echo off
 
-REM cd C:\Users\gtrev\Source\Repos\AzureDevOpsAgileManifestoWidget\vsixArtefacts\packages\Microsoft.VSSDK.BuildTools.15.8.3252\tools\vssdk\bin
+rem https://docs.microsoft.com/en-us/visualstudio/extensibility/walkthrough-publishing-a-visual-studio-extension-via-command-line?view=vs-2017
+
+echo on
 
 d:
 
-cd D:\_Dgit\_TEMPLATES\repub\AzureDevOpsAgileManifestoWidget\vsixArtefacts\packages\Microsoft.VSSDK.BuildTools.15.8.3252\tools\vssdk\bin   
+cd C:\_git\AzureDevOpsAgileManifestoWidget\vsixArtefacts\packages\Microsoft.VSSDK.BuildTools.15.8.3252\tools\vssdk\bin
 
-.\VsixPublisher.exe publish  -payload "..\..\..\..\..\..\AzureDevOpsAgileManifestoWidget\bin\debug\AzureDevOpsAgileManifestoWidget.vsix"  -publishManifest "..\..\..\..\..\..\vsixArtefacts\VsmpPublish.json"  -personalAccessToken "vsmp_pat"
+
+
+.\VsixPublisher.exe publish  -payload "..\..\AgileManifestoWidget\bin\debug\AgileManifestoWidget.vsix"  -publishManifest "..\..\vsixArtefacts\VsmpPublish.json"  -personalAccessToken "vsmp_pat"
