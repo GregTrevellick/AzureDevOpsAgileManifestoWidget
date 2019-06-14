1363ea0414c1dd3abd1936482dbb54c2e9454566
Greg Trevellick <gtrevellick@googlemail.com>
2019-06-14 21:31:07 +0100

    ,

--- Src/AgileManifestoWidget/app/Scripts/configuration222.js
+++ Src/AgileManifestoWidget/app/Scripts/configuration222.js
@@ -12,36 +12,20 @@ VSS.require("TFS/Dashboards/WidgetHelpers", function (WidgetHelpers) {
         return {
 
             load: function (widgetConfigurationContext) {
-                //$updatePreview.click(function () {
-                    var eventName = WidgetHelpers.WidgetEvent.ConfigurationChange;
-                    var eventArgs = WidgetHelpers.WidgetEvent.Args(getCustomSettings());
-                    widgetConfigurationContext.notify(eventName, eventArgs);
-                //});
+                var eventName = WidgetHelpers.WidgetEvent.ConfigurationChange;
+                var eventArgs = WidgetHelpers.WidgetEvent.Args(getCustomSettings());
+                widgetConfigurationContext.notify(eventName, eventArgs);
                 return WidgetHelpers.WidgetStatusHelper.Success();
             },
 
             onSave: function () {
-                //if (!validate()) return false;
                 return WidgetHelpers.WidgetConfigurationSave.Valid(getCustomSettings());
             }
 
         };
 
-        //function validate() {
-        //    if ($url.val() === "") {
-        //        $errorUrl.text("Enter a url to display in the widget");
-        //        $errorUrl.parent().css("visibility", "visible");
-        //        return false;
-        //    }
-        //
-        //    return true;
-        //}
-
         function getCustomSettings() {
             return {
-                //data: JSON.stringify({
-                //    url: $url.val()
-                //})
                 data: ""
             };
         }
